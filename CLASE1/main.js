510b499861907a4aef1fbe8a5d9df3548bfb1020
SantiagoDeCarlucci <santiagodc0110@gmail.com>
2024-10-03 21:05:47 -0300

    Actualizo codigo completo

--- CLASE1/main.js
+++ CLASE1/main.js
@@ -1,45 +1,73 @@
-let nombreUsuario = prompt("Ingrese su nombre")
-console.log (nombreUsuario)
-
-alert ("Bienvenido " +  nombreUsuario + "A continuacion participara del juego")
-
-function pedirNumero() {
-    let numero = parseInt(prompt("Adivina el numero secreto (entre 1 y 100):"));
-
-    while (isNaN(numero) || numero < 1 || numero > 100)  {
-        alert("Ingresa un numero entre 1 y 100.");
-        numero = parseInt(prompt("Adivina el numero secreto (entre 1 y 100):"));
+class Jugador {
+    constructor(nombre, precision) {
+        this.nombre = nombre;
+        this.precision = precision; 
     }
 
-    return numero; 
-}
-
-
-function iniciarJuego() {
-    const numeroSecreto = Math.floor(Math.random() * 100) + 1; 
-    let intentos = 0;
-    let adivinanza = null;
-
-    while (adivinanza !== numeroSecreto) {
-        adivinanza = pedirNumero(); 
-        intentos++;
-
-        if (adivinanza < numeroSecreto) {
-            alert("El numero es mayor. ¡Ssegui intentando!");
-        } else if (adivinanza > numeroSecreto) {
-            alert("El numero es menor. ¡Segui intentando!");
+    patearPenal(portero) {
+        const resultado = Math.random() * 100;
+        if (resultado < this.precision) {
+            alert(`${this.nombre } hace un gol!`);
+            return true;
+        } else if (resultado < portero.habilidad) {
+            alert(`${ portero.nombre } ataja el penal de ${ this.nombre }!`);
+            return false;
         } else {
-            alert("¡Felicidades! " + nombreUsuario + " Adivinaste el numero " + numeroSecreto + " en " + intentos + " intentos.");
+            alert(`${ this.nombre } falla el penal!`);
+            return false;
         }
     }
+}
 
-    let jugarDeNuevo = prompt("¿Queres jugar denuevo? (si/no)").toLowerCase();
-    if (jugarDeNuevo === "si") {
-        iniciarJuego(); 
-    } else {
-        alert(nombreUsuario + "¡Gracias por jugar! Nos vemos.");
+class Portero {
+    constructor(nombre, habilidad) {
+        this.nombre = nombre;
+        this.habilidad = habilidad; 
     }
 }
 
+function crearJugador() {
+    const nombre = prompt("Ingrese el nombre del jugador:");
+    const precision = parseInt(prompt`(Ingrese la calidad de ${ nombre }(entre 1 y 100): )`);
+    return new Jugador(nombre, precision);
+}
 
-iniciarJuego();
+function crearPortero() {
+    const nombre = prompt("Ingrese el nombre del arquero:");
+    const habilidad = parseInt(prompt`(Ingrese la habilidad de ${ nombre } para atajar(entre 1 y 100): )`);
+    return new Portero(nombre, habilidad);
+}
+
+function tandaPenales(jugadores, portero) {
+    let goles = 0;
+    jugadores.forEach(jugador => {
+        if (jugador.patearPenal(portero)) {
+            goles++;
+        }
+    });
+    return goles;
+}
+
+alert("Bienvenido al simulador de penales");
+
+const cantidadJugadores = parseInt(prompt("Cuantos jugadores patean los penales?"));
+const jugadores = [];
+
+for (let i = 0; i < cantidadJugadores; i++) {
+    const jugador = crearJugador();
+    jugadores.push(jugador);
+}
+
+const portero = crearPortero();
+
+alert("Comienza la tanda de penales");
+const golesTanda1 = tandaPenales(jugadores, portero);
+alert`(En la primera tanda, se hicieron ${ golesTanda1 } goles.)`;
+
+
+if (confirm("Queres jugar otra tanda de penales?")) {
+    const golesTanda2 = tandaPenales(jugadores, portero);
+    alert`(En la segunda tanda, se hicieron ${ golesTanda2 } goles.)`;
+} else {
+    alert("Gracias por jugar");
+}
